1cf75f94ab499f7046e3b43a56b114be74f3a092
harish sarwar <mdharishsarwar78@gmail.com>
2024-11-16 13:59:25 +0530

    data tranformation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.ML_Project.logger import logging
 from src.ML_Project.exception import CustomException
 from src.ML_Project.components.data_ingestion import DataIngestion
 from src.ML_Project.components.data_ingestion import DataIngestionConfig
+from src.ML_Project.components.data_transformation import DataTransformationConfig, DataTransformation
 import sys
 
 
@@ -12,7 +13,11 @@ if __name__=="__main__":
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config = DataTransformationConfig()
+        data_transformation = DataTransformation()
+        data_transformation.intiate_data_transformation(train_data_path,test_data_path)
 
     except Exception as e:
         logging.info("Custom Exception")
