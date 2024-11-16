ebcbe550a4c4af24e4757b5d9a4804a629dc829c
harish sarwar <mdharishsarwar78@gmail.com>
2024-11-16 16:21:41 +0530

    model trainer

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from src.ML_Project.exception import CustomException
 from src.ML_Project.components.data_ingestion import DataIngestion
 from src.ML_Project.components.data_ingestion import DataIngestionConfig
 from src.ML_Project.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.ML_Project.components.model_trainer import ModelTrainer,ModelTrainerConfig
+
+
 import sys
 
 
@@ -17,7 +20,12 @@ if __name__=="__main__":
 
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.intiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_ = data_transformation.intiate_data_transformation(train_data_path,test_data_path)
+
+        ## Model Taining
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_tranier(train_array=train_arr,test_array=test_arr))
 
     except Exception as e:
         logging.info("Custom Exception")
